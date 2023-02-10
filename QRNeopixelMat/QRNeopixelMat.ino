df7c5220d1f78173da4e1a259dd73dbfca489117
JOSE <joseangeldelangel10@gmail.com>
2023-02-09 23:28:21 -0600

    added matrix plus ota code

--- QRNeopixelMat/QRNeopixelMat.ino
+++ QRNeopixelMat/QRNeopixelMat.ino
@@ -8,9 +8,19 @@ following tags:
         ...
         _______________________ END OF YOUR CODE LIBRARIES _______________________
 
+        _______________________ YOUR CODE CONSTANTS _______________________
+        ...
+        _______________________ END OF YOUR CODE CONSTANTS _______________________
+
+        _______________________ YOUR CODE SETUP _______________________
+        ...
+        _______________________ END OF YOUR CODE SETUP _______________________
+        
         _______________________ YOUR CODE _______________________
         ...
         _______________________ END OF YOUR CODE _______________________
+
+        
 */
 
 #include <WiFi.h>
@@ -21,9 +31,40 @@ following tags:
 
 // _______________________ YOUR CODE LIBRARIES _______________________
 
-// #include <someNewLibrary.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
 
-//_______________________ END OF YOUR CODE LIBRARIES _______________________
+// _______________________ END OF YOUR CODE LIBRARIES _______________________
+
+
+// _______________________ YOUR CODE CONSTANTS _______________________
+
+#define PIN 23
+#define LED_RED_HIGH     (31 << 11)
+#define LED_GREEN_HIGH     (63 << 5)  
+
+#define LED_BLUE_HIGH     31
+const int pinx = 25;
+const int piny = 26;
+const int pinz = 33;
+
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, 1, 1, PIN,
+  NEO_TILE_TOP   + NEO_TILE_RIGHT   + NEO_TILE_ROWS   + NEO_TILE_PROGRESSIVE +
+  NEO_MATRIX_TOP + NEO_MATRIX_RIGHT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+int x    = matrix.width();
+int pass = 0;
+
+// _______________________ END OF YOUR CODE CONSTANTS _______________________
 
 const char *host = "esp32";
 const char *ssid = "QR_ESP32_Wifi";
@@ -226,6 +267,16 @@ void setup(void)
             }
         });
     server.begin();
+
+    // _______________________ YOUR CODE SETUP _______________________
+    matrix.begin();
+    matrix.setTextWrap(false);
+    matrix.setBrightness(20);
+    matrix.setTextColor(colors[2]);
+    pinMode(pinx, INPUT);
+    pinMode(piny, INPUT);
+    pinMode(pinz, INPUT);
+    // _______________________ END OF YOUR CODE SETUP _______________________
 }
 
 void loop(void)
@@ -235,7 +286,45 @@ void loop(void)
 
     // _______________________ YOUR CODE _______________________
 
-    // someNewCode.run()
+    int estx = digitalRead(pinx);
+    int esty = digitalRead(piny);
+    int estz = digitalRead(pinz);
+    if (estz == 0 && esty == 0 && estx == 0                                                  ){
+       matrix.fillScreen(0);
+       matrix.show();
+    }
+    else if (estz == 0 && esty == 0 && estx == 1){
+      matrix.fillScreen(colors[2]);
+      matrix.show();
+    }
+    else if (estz == 0 && esty == 1 && estx == 0 ){
+      matrix.fillScreen(colors[0]);
+      matrix.show();
+    
+    }  
+    else if (estz == 0 && esty == 1 && estx == 1){
+      matrix.fillScreen(colors[1]);
+      matrix.show();
+      delay(1000);
+      matrix.fillScreen(0);
+      matrix.show();
+      delay(1000);
+    }
+    else if (estz == 1 && esty == 0 && estx == 0){
+    matrix.fillScreen(0);
+    matrix.setCursor(x, 0);
+    matrix.print(F("Q"));
+    matrix.setRotation(1);
+    if(--x < -96) {
+      x = matrix.width();
+    }
+    matrix.show();
+    delay(250);
+    }
+    else{
+      matrix.fillScreen(0);
+      matrix.show();
+    }
 
     // _______________________ END OF YOUR CODE _______________________
 }
